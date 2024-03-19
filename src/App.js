b036a8e2fb58bcd087a284bae1f6d292a39e4ad0
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-03-19 13:07:54 +0200

    CREATE: initial project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="contained">Type</Button>
+    </div>
+  );
+};
+
+export default App;
